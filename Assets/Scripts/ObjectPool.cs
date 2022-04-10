12c5bf6e623d484f4ab5264146be153f8d98b65b
Jo Sunhyeon <35026790+josunhyeon@users.noreply.github.com>
2022-04-10 18:51:00 +0900

    Add initial files.

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,39 @@
+// References:
+// https://learn.unity.com/tutorial/live-training-shop-ui-with-runtime-scroll-lists#5c7f8528edbc2a002053b4c8
+// Paris Buttfield-Addison, Jon Manning, and Tim Nugent. Unity Game Development Cookbook: Essentials for Every Game. O'Reilly Media, 2019
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+	public GameObject GetObject() 
+	{
+		if (inactiveObjects.Count > 0) 
+		{
+			var dequeuedObject = inactiveObjects.Dequeue();
+			dequeuedObject.transform.SetParent(null);
+			dequeuedObject.SetActive(true);
+			return dequeuedObject;
+		}
+		else 
+		{
+			return Instantiate(prefab);
+		}
+	}
+
+	public void ReturnObject(GameObject toReturn) 
+	{
+		if (toReturn == null)
+		{
+			throw new ArgumentNullException();
+		}
+		toReturn.transform.SetParent(transform);
+		toReturn.SetActive(false);
+		inactiveObjects.Enqueue(toReturn);
+	}
+	
+	public GameObject prefab;
+	private Queue<GameObject> inactiveObjects = new Queue<GameObject>();
+}
